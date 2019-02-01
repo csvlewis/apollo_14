33ca15ec5d1fa57568e3e0fb90629092bafc2c0a
Chris Lewis <13chlewis@gmail.com>
2019-02-01 10:52:23 -0700

    User Story 3 complete

--- spec/features/astronaut_index_spec.rb
+++ spec/features/astronaut_index_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'when a visitor visits the astronaut index page', type: :feature do
   before :each do
-    @mission_1 = Mission.create(title: 'Apollo 14', time_in_space: 5)
-    @mission_2 = Mission.create(title: 'Armageddon', time_in_space: 15)
+    @mission_1 = Mission.create(title: 'Armageddon', time_in_space: 15)
+    @mission_2 = Mission.create(title: 'Apollo 14', time_in_space: 5)
     @astronaut_1 = Astronaut.create(name: 'Alan Shepard', age: 40, job: 'Commander', missions: [@mission_1, @mission_2] )
-    @astronaut_2 = Astronaut.create(name: 'Stuart Roosa', age: 25, job: 'Command Module Pilot', missions: [@mission_1])
-    @astronaut_3 = Astronaut.create(name: 'Edgar Mitchell', age: 25, job: 'Lunar Module Pilot', missions: [@mission_1] )
+    @astronaut_2 = Astronaut.create(name: 'Stuart Roosa', age: 25, job: 'Command Module Pilot', missions: [@mission_2])
+    @astronaut_3 = Astronaut.create(name: 'Edgar Mitchell', age: 25, job: 'Lunar Module Pilot', missions: [@mission_2] )
   end
   it 'can see a list of astronauts, each with a name, age and job' do
 
@@ -26,8 +26,16 @@ RSpec.describe 'when a visitor visits the astronaut index page', type: :feature 
     expect(page).to have_content("Job: #{@astronaut_3.job}")
 
   end
+
   it 'can see the average age of all astronauts' do
     visit astronauts_path
+
     expect(page).to have_content("Average age: 30.0")
+  end
+
+  it 'can see a list of each astronaut\'s missions in alphabetical order' do
+    visit astronauts_path
+
+    expect(page).to have_content("Missions:\nArmageddon\nApollo 14")
   end
 end
